0995a2e0f29d5da4be1a424c26efe776ad470581
Almaz <almazius11@gmail.com>
2023-03-09 13:31:25 +0300

    created base structure

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './index.scss'
+import { Success } from './components/Success'
+import { Users } from './components/Users'
+
+// https://reqres.in/api/users
+
+function App() {
+  return (
+    <div className="App">
+      <Users />
+      {/* <Success /> */}
+    </div>
+  )
+}
+
+export default App
